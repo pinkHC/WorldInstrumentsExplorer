c9e66645468cca188fcc38262fef5a8c7707b76b
Frank Fan <pink8uragano@gmail.com>
2026-02-07 21:21:16 +0800

    Initial Commit

---
+++ WorldInstrumentsExplorer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorldInstrumentsExplorer
+//
+//  Created by zhengbei on 2026/2/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorldInstrumentsExplorer/WorldInstrumentsExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldInstrumentsExplorerApp.swift
+//  WorldInstrumentsExplorer
+//
+//  Created by zhengbei on 2026/2/7.
+//
+
+import SwiftUI
+
+@main
+struct WorldInstrumentsExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
